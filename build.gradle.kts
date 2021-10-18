8e7a98ca1e9fd9e73f39c1c15dbf761896b03cc6
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2021-10-18 14:40:38 +0200

    Initial project setup

---
+++ build.gradle.kts
@@ -0,0 +1,130 @@
+plugins {
+    id("com.github.johnrengelman.shadow") version "7.0.0"
+    java
+    `maven-publish`
+}
+
+group = "de.eldoria"
+version = "1.0"
+val publishData = PublishData(project)
+val shadebase = "de.eldoria.gridselector.libs"
+
+repositories {
+    maven("https://eldonexus.de/repository/maven-public/")
+    maven("https://eldonexus.de/repository/maven-proxies/")
+}
+
+dependencies {
+    implementation("de.eldoria", "eldo-util", "1.10.3")
+    compileOnly("de.eldoria", "schematicbrushreborn", "1.5.0-20210919.131332-5")
+    compileOnly("org.spigotmc", "spigot-api", "1.16.5-R0.1-SNAPSHOT")
+
+    testImplementation("org.junit.jupiter", "junit-jupiter-api", "5.6.0")
+    testRuntimeOnly("org.junit.jupiter", "junit-jupiter-engine")
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+    sourceCompatibility = JavaVersion.VERSION_11
+}
+
+publishing {
+    publications.create<MavenPublication>("maven") {
+        artifact(tasks["shadowJar"])
+        artifact(tasks["sourcesJar"])
+        artifact(tasks["javadocJar"])
+        groupId = project.group as String?
+        artifactId = project.name.toLowerCase()
+        version = publishData.getVersion()
+    }
+
+    repositories {
+        maven {
+            authentication {
+                credentials(PasswordCredentials::class) {
+                    username = System.getenv("NEXUS_USERNAME")
+                    password = System.getenv("NEXUS_PASSWORD")
+                }
+            }
+
+            name = "EldoNexus"
+            url = uri(publishData.getRepository())
+        }
+    }
+}
+
+tasks {
+    compileJava {
+        options.encoding = "UTF-8"
+    }
+
+    compileTestJava{
+        options.encoding = "UTF-8"
+    }
+    test {
+        useJUnitPlatform()
+        testLogging {
+            events("passed", "skipped", "failed")
+        }
+    }
+    shadowJar{
+        relocate("de.eldoutilities", shadebase + "eldoutilities")
+        mergeServiceFiles()
+    }
+    processResources {
+        from(sourceSets.main.get().resources.srcDirs) {
+            filesMatching("plugin.yml") {
+                expand(
+                    "version" to project.version
+                )
+            }
+            duplicatesStrategy = DuplicatesStrategy.INCLUDE
+        }
+    }
+        register<Copy>("copyToServer") {
+        val path = project.property("targetDir") ?: "";
+        if (path.toString().isEmpty()) {
+            println("targetDir is not set in gradle properties")
+            return@register
+        }
+        from(shadowJar)
+        destinationDir = File(path.toString())
+    }
+}
+
+class PublishData(private val project: Project) {
+    var type: Type = getReleaseType()
+    var hashLength: Int = 7
+
+    private fun getReleaseType(): Type {
+        val branch = getCheckedOutBranch()
+        return when {
+            branch.contentEquals("master") -> Type.RELEASE
+            branch.startsWith("dev") -> Type.DEV
+            else -> Type.SNAPSHOT
+        }
+    }
+
+    private fun getCheckedOutGitCommitHash(): String = System.getenv("GITHUB_SHA")?.substring(0, hashLength) ?: "local"
+
+    private fun getCheckedOutBranch(): String = System.getenv("GITHUB_REF")?.replace("refs/heads/", "") ?: "local"
+
+    fun getVersion(): String = getVersion(false)
+
+    fun getVersion(appendCommit: Boolean): String =
+        type.append(getVersionString(), appendCommit, getCheckedOutGitCommitHash())
+
+    private fun getVersionString(): String = (project.version as String).replace("-SNAPSHOT", "").replace("-DEV", "")
+
+    fun getRepository(): String = type.repo
+
+    enum class Type(private val append: String, val repo: String, private val addCommit: Boolean) {
+        RELEASE("", "https://eldonexus.de/repository/maven-releases/", false),
+        DEV("-DEV", "https://eldonexus.de/repository/maven-dev/", true),
+        SNAPSHOT("-SNAPSHOT", "https://eldonexus.de/repository/maven-snapshots/", true);
+
+        fun append(name: String, appendCommit: Boolean, commitHash: String): String =
+            name.plus(append).plus(if (appendCommit && addCommit) "-".plus(commitHash) else "")
+    }
+}
