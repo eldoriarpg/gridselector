8d74d0c08e52a41002065d43795861293fd07bc0
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2021-11-05 18:58:29 +0100

    Add save schematics command

--- src/main/java/de/eldoria/gridselector/command/Grid.java
+++ src/main/java/de/eldoria/gridselector/command/Grid.java
@@ -4,17 +4,21 @@ import de.eldoria.eldoutilities.commands.command.AdvancedCommand;
 import de.eldoria.eldoutilities.commands.command.CommandMeta;
 import de.eldoria.gridselector.command.grid.DefineGrid;
 import de.eldoria.gridselector.command.grid.DrawGrid;
+import de.eldoria.gridselector.command.grid.SaveSchematics;
 import de.eldoria.gridselector.command.grid.Select;
 import de.eldoria.gridselector.config.Configuration;
 import de.eldoria.gridselector.listener.SelectionListener;
+import de.eldoria.gridselector.schematics.GridSchematics;
+import de.eldoria.schematicbrush.SchematicBrushReborn;
 import org.bukkit.plugin.Plugin;
 
 public class Grid extends AdvancedCommand {
-    public Grid(Plugin plugin, SelectionListener selectionListener, Configuration config) {
+    public Grid(Plugin plugin, SchematicBrushReborn schematicBrushReborn, SelectionListener selectionListener, Configuration config, GridSchematics gridSchematics) {
         super(plugin, CommandMeta.builder("schematicbrushgrid")
                 .withSubCommand(new DefineGrid(plugin, config))
                 .withSubCommand(new Select(plugin, selectionListener))
                 .withSubCommand(new DrawGrid(plugin, config))
+                .withSubCommand(new SaveSchematics(plugin, schematicBrushReborn, gridSchematics))
                 .build());
     }
 }
---
+++ src/main/java/de/eldoria/gridselector/command/grid/SaveSchematics.java
@@ -0,0 +1,2 @@
+package de.eldoria.gridselector.command.grid;public class SaveSchematics {
+}
