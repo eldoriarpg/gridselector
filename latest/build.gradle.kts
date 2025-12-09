7ca19e69c4c5204b052cec37d95c581780426a81
Nora <46890129+RainbowDashLabs@users.noreply.github.com>
2025-12-09 17:05:29 +0100

    Fix shade base

--- latest/build.gradle.kts
+++ latest/build.gradle.kts
@@ -40,7 +40,8 @@ tasks {
         val shadebase = "de.eldoria.schematicbrush.libs."
         relocate("de.eldoria.messageblocker", shadebase + "messageblocker")
         relocate("com.fasterxml", shadebase + "fasterxml")
-        relocate("de.eldoria.utilities", shadebase + "utilities")
+        relocate("de.eldoria.jacksonbukkit", shadebase + "jacksonbukkit")
+        relocate("de.eldoria.eldoutilities", shadebase + "utilities")
     }
 
     build {
